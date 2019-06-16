8be09e9c4e1aef4a947a411adbc90b52427e4a99
Reyhan Rishard <s182355@students.aie.edu.au>
2019-06-17 00:46:16 +1000

    binary maths

---
+++ Maths Library/StaticLib1/StaticLib1/Binary.cpp
@@ -0,0 +1,12 @@
+#include "Binary.h"
+
+
+
+Binary::Binary()
+{
+}
+
+
+Binary::~Binary()
+{
+}
---
+++ Maths Library/StaticLib1/StaticLib1/Binary.h
@@ -0,0 +1,9 @@
+namespace Binary
+{
+	int dec_to_bin(char* binary_string, int value) 
+	{
+
+	}
+
+
+}
